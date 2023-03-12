4270af4f7a25003cf25c224c965413061310f17d
Adam Chu <achu.inet@gmail.com>
2023-03-12 15:02:45 -0700

    trial publish

--- newsgen.py
+++ newsgen.py
@@ -25,6 +25,7 @@ with open('index.html', 'w') as f:
     f.write('<!DOCTYPE html>\n')
     f.write('<html>\n')
     f.write('<head>\n')
+    f.write('<meta charset="UTF-8">\n')
     f.write('<title>阿媽新聞匯</title>\n')
     f.write('<style>\n')
     f.write('  #title {\n')
@@ -46,7 +47,8 @@ with open('index.html', 'w') as f:
 
     # Write the HTML code for the links
     for news_link in news_links:
-        f.write(news_link + '<br>\n')
+        f.write(news_link)
+        f.write('<br>\n')
 
     # Write the HTML footer
     f.write('</body>\n')
