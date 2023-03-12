fb2b510cc3ab66aec9bcc9e0af4158124beb78fc
Adam Chu <achu.inet@gmail.com>
2023-03-12 14:27:25 -0700

    add title to homepage

--- newsgen.py
+++ newsgen.py
@@ -21,5 +21,18 @@ for url in urls:
 
 # Write the generated HTML code to a new output file
 with open('index.html', 'w') as f:
+    # Write the HTML header, including the title
+    f.write('<!DOCTYPE html>\n')
+    f.write('<html>\n')
+    f.write('<head>\n')
+    f.write('<title>阿媽新聞匯</title>\n')
+    f.write('</head>\n')
+    f.write('<body>\n')
+
+    # Write the HTML code for the links
     for news_link in news_links:
         f.write(news_link + '<br>\n')
+
+    # Write the HTML footer
+    f.write('</body>\n')
+    f.write('</html>\n')
